afe2079122a338f0797135cfc1b44aac7a6e2e03
Petemab <peteben18@hotmail.com>
2018-04-05 12:31:24 +0100

    tring to add footer and refractor

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 $(function(){
 
-  //
+  // A massive list of variables
   const $holes = $('.hole');
   const $scoreP1 = $('#scoreP1');
   const $scoreP2 = $('#scoreP2');
@@ -8,35 +8,29 @@ $(function(){
   const $medium = $('#medium');
   const $insane = $('#insane');
   const $startButton = $('.startButton');
-  // const $highlightedHole = $('.highlightedHole');
   const $resetBtn = $('.resetBtn');
   const $timer = $('.countdown');
-  let timeUp = false;
-  let lastHole;
-  let tally = 0;
-  let difficulty = '';
-  let timeRemaining = 15;
-  let time;
-  let gameStarted = false;
-  let startGame1 = false;
-  // let reset = false;
-  // const startGame2 = false;
-  // let timerValue = null;
-  // let timeIsRunning = false;
   const $howModal = $('#howModal');
   const $howToPlay = $('#howToPlay');
   const $close = $('.close');
-  const $player1Score = $('#player1Score');
+  const $player1PopUp = $('#player1PopUp');
   const $p1ScoreDisplay = $('.p1ScoreDisplay');
   const $player2Start = $('#player2Start');
-  const $player2Score = $('#player2Score');
+  const $player2PopUp = $('#player2PopUp');
   const $p2ScoreDisplay = $('.p2ScoreDisplay');
   const $playAgain = $('.playAgain');
   const $winner = $('.winner');
   const $bomb = new Audio('./sfx/explosion.mp3');
   const $laser = new Audio('./sfx/Laser.mp3');
-
-
+  let timeUp = false;
+  let lastHole;
+  let p1Score = 0;
+  let p2Score = 0;
+  let difficulty = '';
+  let timeRemaining = 15;
+  let time;
+  let gameStarted = false;
+  let startGame1 = false;
 
 
   //pop up insruction screen
@@ -45,60 +39,58 @@ $(function(){
     $laser.play();
   });
 
+  //close the pop instruction display
   $close.on('click', function(){
     $howModal.css('display', 'none');
 
   });
-  //Start button
+  //Start button to kick off the main game
   $startButton.on('click', function(){
     startGame();
+    $('.game').on('click', function(){
+      $laser.play();
+    });
   });
 
+  //Starts player 2 games and closes the pop up modal with P1 score
   $player2Start.on('click', function(){
-    $player1Score.css('display', 'none');
+    $player1PopUp.css('display', 'none');
     startGame2();
   });
 
   $playAgain.on('click', function(){
-    $player2Score.css('display', 'none');
+    $player2PopUp.css('display', 'none');
   });
 
-  //levels
+  //difficulty level - pretty straightforward
   $easy.on('click', function(){
     difficulty = 'easy';
-    console.log(difficulty);
   });
   $medium.on('click', function(){
     difficulty = 'medium';
-    console.log(difficulty);
   });
   $insane.on('click', function(){
     difficulty = 'insane';
-    console.log(difficulty);
-
   });
 
-
+  // starts and then stops the countdown display when the game begins
   function countdown(){
     const countdown = setInterval(function() {
       timeRemaining--;
-      console.log(timeRemaining);
       $timer.text(timeRemaining);
       if(timeRemaining === 0){
-        console.log('Stop counting down now please');
         clearInterval(countdown);
         gameStarted = false;
       }
     }, 1000);
-
   }
 
-  // random time generator
+  // random time generator - finds a random number of milliseconds based on the level chosen
   function randomTime(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
   }
-  // console.log(randomTime(200, 800));
 
+  // selects a random and then should make sure the same hole isn't selected twice in a row
   function randomHole($holes) {
     const index = Math.floor(Math.random() * $holes.length);
     const selectedHole = $holes[index];
@@ -109,7 +101,8 @@ $(function(){
     return selectedHole;
   }
 
-
+  // 'Highlights' different balls (makes them coloured and vibrate) by changing
+  // the class . Speed based on the difficulty level selected.
   function highlight(){
     if(difficulty === 'easy'){
       time = randomTime(800, 1000);
@@ -120,105 +113,96 @@ $(function(){
     } else {
       time = randomTime(700, 1000);
     }
-    console.log(time);
-    // const time = randomTime(600, 1000);
     const hole = randomHole($holes);
-    // console.log(time, hole);
     hole.classList.add('highlightedHole');
     setTimeout(() => {
       hole.classList.remove('highlightedHole');
       if(!timeUp)highlight();
     }, time);
   }
-  // highlight();
 
+  // starts the first player's game and brings up the score on time up
   function startGame(){
     if(!gameStarted){
-      $('.game').on('click', function(){
-        $laser.play();
-      });
       gameStarted = true;
       startGame1 = true;
       $scoreP1.text(0);
       timeUp = false;
-      tally = 0;
+      p1Score = 0;
       highlight();
       countdown();
       setTimeout(() => {
         timeUp = true;
-        $player1Score.css('display', 'block');
-        $p1ScoreDisplay.text(tally);
+        $player1PopUp.css('display', 'block');
+        $p1ScoreDisplay.text(p1Score);
       },15000);
     }
-
-    console.log(timeUp);
-
   }
 
+  // starts the second player's game if the first player has finished
   function startGame2(){
     if(!gameStarted){
-      $('.game').on('click', function(){
-        $laser.play();
-      });
       timeRemaining = 15;
       startGame1 = false;
       gameStarted = true;
       $scoreP2.text(0);
       timeUp = false;
-      tally = 0;
+      p2Score = 0;
       highlight();
       countdown();
-      setTimeout(() => {
-        timeUp = true;
-        $player2Score.css('display', 'block');
-        $p2ScoreDisplay.text(tally);
-        if ($scoreP1 === $scoreP2){
-          $winner.text('It\'s a draw!');
-        } else if ($scoreP1 > $scoreP2){
-          $winner.text('Player 1 wins! You need more practice Player 2');
-        } else {
-          $winner.text('Player 2 wins! You need more practice Player 1');
-        }
-      },15000);
+      findWinner();
     }
-
-    console.log(timeUp);
-
   }
 
+  // finds the winner once the set time out is complete
+  function findWinner(){
+    setTimeout(() => {
+      timeUp = true;
+      $player2PopUp.css('display', 'block');
+      $p2ScoreDisplay.text(p2Score);
+      if (p1Score === p2Score){
+        $winner.text('It\'s a draw!');
+      } else if (p1Score > p2Score){
+        $winner.text('Player 1 wins! You need more practice Player 2');
+      } else {
+        $winner.text('Player 2 wins! You need more practice Player 1');
+      }
+    },15000);
+  }
 
-  // EVENT DELEGATION??
-
+  // When the game is in play and the highlighted hole class is called you can
+  // click on it and it adds to score (also includes sfx)
   $('.game').on('click', '.highlightedHole',  function() {
     if(startGame1){
-      tally++;
-      console.log(tally);
-      $scoreP1.text(tally);
+      p1Score++;
+      $scoreP1.text(p1Score);
       $bomb.play();
     } else {
-      tally++;
-      $scoreP2.text(tally);
+      p2Score++;
+      $scoreP2.text(p2Score);
       $bomb.play();
     }
   });
 
+  //Reset button. Just reloads the page. I broke it repeatedly trying to reset properly!
   $resetBtn.on('click', function(){
     location.reload();
-    // // reset = true;
-    // timeUp = false;
-    // tally = 0;
-    // $scoreP1.text(tally);
-    // $scoreP2.text(tally);
-    // difficulty = '';
-    // timeRemaining = 15;
-    // startGame1 = false;
-    // gameStarted = false;
-    // $timer.text(timeRemaining);
   });
 
-
-
 });
+
+
+// // reset = true;
+// timeUp = false;
+// tally = 0;
+// $scoreP1.text(tally);
+// $scoreP2.text(tally);
+// difficulty = '';
+// timeRemaining = 15;
+// startGame1 = false;
+// gameStarted = false;
+// $timer.text(timeRemaining);
+
 
 
 // if(!timeUp){
